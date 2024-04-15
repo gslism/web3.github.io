a54b3b490beb0c3b8877b7440a81a253c110759d
s0177158 <elisavetastyrchak@gmail.com>
2024-04-15 19:13:21 +0300

    ffffdff

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                 <div class="col-auto">
                   <label for="validationCustom01" class="form-label">Фамилия Имя Отчество:</label>
                   <input type="text" placeholder="fio" name="login" class="form-control rounded-pill" id="validationCustom01" value=""  />
-                  <span class="error">* <?php echo $Login_Error;?></span>
+                  
                 </div>
           
                 <div class="col-auto">
